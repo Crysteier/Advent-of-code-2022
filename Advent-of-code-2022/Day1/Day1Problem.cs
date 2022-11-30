89f741fd82603baaac3a8f27e8dda3443aa84bd7
Adam Moricz <adam.moricz@gmail.com>
2022-11-30 13:01:12 +0100

    Add project files.

---
+++ Advent-of-code-2022/Day1/Day1Problem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_code_2022.Day1
+{
+    public class Day1Problem
+    {
+        public string input;
+        public Day1Problem(string input)
+        {
+            this.input = input;
+        }
+    }
+}
