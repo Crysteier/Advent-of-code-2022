07da2c819f56d48d7877b5f4b07500d7da8e85ae
Adam Moricz <adam.moricz@gmail.com>
2022-12-04 21:01:57 +0100

    day 4 done, fuck me I am stupid

---
+++ Advent-of-code-2022/Days/Day4.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_code_2022.Days
+{
+    public class Day4 : BaseDay, IDay
+    {
+        public Day4(string inputPath) : base(inputPath) { }
+
+        public void SolvePuzzlePart1()
+        {
+            char[] delimeters = { ',', '-' };
+            int sum = 0;
+            foreach (var item in input)
+            {
+                var sections = item.Split(delimeters);
+                if ((int.Parse(sections[0]) >= int.Parse(sections[2])) && (int.Parse(sections[1]) <= int.Parse(sections[3])))
+                {
+                    sum++;
+                }
+                else if (int.Parse(sections[0]) <= int.Parse(sections[2]) && int.Parse(sections[1]) >= int.Parse(sections[3]))
+                {
+                    sum++;
+                }
+            }
+
+            Console.WriteLine("Puzzle 1 solution: " + sum);
+        }
+
+        public void SolvePuzzlePart2()
+        {
+            char[] delimeters = { ',', '-' };
+            int sum = 0;
+            foreach (var item in input)
+            {
+                var sections = item.Split(delimeters);
+                var numeroUno = int.Parse(sections[0]);
+                var numeroDos = int.Parse(sections[1]);
+                var numeroTres = int.Parse(sections[2]);
+                var numeroQuatro = int.Parse(sections[3]);
+                if ((Enumerable.Range(numeroUno, Math.Abs(numeroDos - numeroUno + 1)).Contains(numeroTres)) || (Enumerable.Range(numeroUno, Math.Abs(numeroDos - numeroUno + 1)).Contains(numeroQuatro)))
+                {
+                    sum++;
+                }
+                else if (Enumerable.Range(numeroTres, Math.Abs(numeroQuatro - numeroTres + 1)).Contains(numeroUno) || (Enumerable.Range(numeroTres, Math.Abs(numeroQuatro - numeroTres + 1)).Contains(numeroDos)))
+                {
+                    sum++;
+                }
+            }
+
+            Console.WriteLine("Puzzle 2 solution: " + sum);
+        }
+
+        public void SolvePuzzles()
+        {
+            SolvePuzzlePart1();
+            SolvePuzzlePart2();
+        }
+    }
+}
--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Advent_of_code_2022.Day1;
 using Advent_of_code_2022.Days;
 
-var day = new Day3("inputs/day3.txt");
+var day = new Day4("inputs/day4.txt");
 
 day.SolvePuzzles();
